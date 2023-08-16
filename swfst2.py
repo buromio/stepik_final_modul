81aca6bb4337d624c9680c878f4b585c7d549143
buromio <buromio@gmail.com>
2023-08-16 14:50:07 +0500

    Added an exception to the 'base_page.py' file

--- swfst2.py
+++ swfst2.py
@@ -15,4 +15,10 @@ def browser():
     browser.quit()
 
 
+def test_guest_can_go_to_login_page(browser):
+   browser.get(link)
+   go_to_login_page(browser)
 
+def go_to_login_page(browser):
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
