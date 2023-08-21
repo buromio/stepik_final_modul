71afb605071893d1ad5ba3cca58ca5eba7c6df93
buromio <buromio@gmail.com>
2023-08-21 13:12:25 +0500

    Added a check for the authorization and registration form detection

--- swfst2.py
+++ swfst2.py
@@ -22,3 +22,16 @@ def test_guest_can_go_to_login_page(browser):
 def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "URL does not contain 'login' substring"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not present on the login page"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not present on the login page"
+
+    self.should_be_login_url()
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "URL does not contain 'login' substring"
